455416d556106ebe6f916b7f9c68128a3b92624e
liaoyu <aoyu.li@fenxi.de>
2024-02-23 17:18:13 +0800

    fix: ceshi

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -238,7 +238,7 @@ const Home: React.FC = () => {
         console.log('current.scrollHeight: ', current.scrollHeight);
     }, [items])
 
-    //测试
+
     useEffect(() => {
         if (messageType.trim() !== '' && currentMessage.trim() !== '') {
             if (messageType === '2') {
